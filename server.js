2598a81552ccce145b76576496c5cdf8375cb84b
Will <willstone@gmail.com>
2021-03-16 23:37:41 +0000

    added const bodyParser & included it with app.use

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
