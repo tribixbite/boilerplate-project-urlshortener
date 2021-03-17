06a33fac2d812a4d88b5bd80417ca3c5000e1dd1
Will <willstone@gmail.com>
2021-03-17 03:38:19 +0000

    added dns lookup and code to modify url into hostname by removing http/https and everything after slash

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
+const dns = require('dns');
+//dns.lookup(url, cb);
+// dns.lookup("techneesssh.com", function(err, address){console.log(address == undefined)}) true if invalid
+
+var savedURLS = {};
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -20,6 +25,29 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.post('/api/shorturl/new', function(req, res) {
+  console.log(req.body.url);
+  let host = req.body.url;
+  let reghttp = /^https?:\/\//;
+  if (reghttp.test(host)){host = host.slice(host.indexOf("//")+2)};
+  if (host.indexOf("/") > 0) {host = host.slice(0, host.indexOf("/"))}
+  //host = host.replace("http://","").replace("https://","").split('/')[0].split('?')[0] prolly a better way to do it...
+  console.log(host);
+  dns.lookup(host, function(err, address){
+    if (err) { 
+      res.json({error: 'invalid url'}); 
+      return console.error(err); 
+      }
+
+    if (address == undefined) {
+      res.json({error: 'invalid url'});
+    }
+    //do some stuff
+    });
+  //res.json({ greeting: 'hello API' });
+});
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
